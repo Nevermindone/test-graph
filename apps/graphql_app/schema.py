5e95ea377d139c080a59642223d2ffcf9d9eed7a
Nevermindone <alexandr.jri.bystrov@yandex.ru>
2024-03-18 02:36:04 +0300

    all realations are working

--- apps/graphql_app/schema.py
+++ apps/graphql_app/schema.py
@@ -2,12 +2,12 @@ import dataclasses
 
 import strawberry
 import strawberry_django
-from django.db.models import ForeignKey, ManyToManyField
+from django.db.models import ManyToManyField
 from strawberry_django.optimizer import DjangoOptimizerExtension
 from strawberry import auto
 
 from . import models
-# from .types import Fruit, Color
+from .types import Fruit, Recipe
 from dataclasses import dataclass, make_dataclass, asdict, field
 import django.apps
 from typing import List
@@ -15,9 +15,8 @@ from typing import List
 
 # @strawberry.type
 # class Query:
-#     fruits: list[Fruit] = strawberry_django.field()
-#     # fruit: Fruit = strawberry_django.field()
-#     # color: Color = strawberry_django.field()
+#     fruits: List[Fruit] = strawberry_django.field()
+#     recipes: List[Recipe] = strawberry_django.field()
 #
 #
 # schema = strawberry.Schema(
@@ -33,76 +32,109 @@ DEVELOPMENT
 
 """
 
-
-
-
-
 def _plural_from_single(s):
     return s.rstrip('y') + 'ies' if s.endswith('y') else s + 's'
 
 
-def model_to_dataclass(model, set_relations=False):
+def create_strawberry_model(fields_for_class, fields_for_filter,  strawberry_model_name, django_model):
+    new_dataclass = type(strawberry_model_name, (), {'__annotations__': fields_for_class})
+    new_filter_dataclass = type(f"{strawberry_model_name}Filter", (), {'__annotations__': fields_for_filter})
+    new_ordering_dataclass = type(f"{strawberry_model_name}Order", (), {'__annotations__': fields_for_filter})
+    # FILTER
+    filters = strawberry_django.filter(model=django_model, lookups=True)
+    filter_model = filters(new_filter_dataclass)
+    # ORDER
+    order = strawberry_django.order(model=django_model)
+    order_model = order(new_ordering_dataclass)
+    # FINAL DATA CLASS
+    wrapper = strawberry_django.type(model=django_model, filters=filter_model, order=order_model, pagination=True)
+
+    return wrapper(new_dataclass), {django_model.__name__: wrapper(new_dataclass)}
+
+
+def create_fields(model, set_relations=False):
 
     model_name = model.__name__
     if not set_relations:
-        model_name = f"{model_name}Relation"
+        model_name = f"{model_name}SetupForRelations"
 
     model_fields = model._meta.get_fields()
     fields_for_class = {}
     fields_for_filter = {}
     for model_field in model_fields:
         relation = model_field.__dict__.get('related_model', None)
+
         is_many_to_many = isinstance(model_field, ManyToManyField)
-        print(model_field, is_many_to_many)
         if relation and set_relations and not is_many_to_many:
             fields_for_class.update(
                 {model_field.name: wrapped_models_dict[model_field.__dict__['related_model'].__name__]}
             )
         elif relation and set_relations and is_many_to_many:
-            print('I AM HERE')
             fields_for_class.update(
                 {model_field.name: List[wrapped_models_dict[model_field.__dict__['related_model'].__name__]]}
             )
         else:
             fields_for_class.update({model_field.name: auto})
             fields_for_filter.update({model_field.name: auto})
-    # CREATING CLASSES FOR DATA, FILTERS, PAGINATION AND SORTING
-    new_dataclass = type(model_name, (), {'__annotations__': fields_for_class})
-    new_filter_dataclass = type(f"{model_name}Filter", (), {'__annotations__': fields_for_filter})
-    new_ordering_dataclass = type(f"{model_name}Order", (), {'__annotations__': fields_for_filter})
-    # FILTER
-    filters = strawberry_django.filter(model=model, lookups=True)
-    filter_model = filters(new_filter_dataclass)
-    # ORDER
-    order = strawberry_django.order(model=model)
-    order_model = order(new_ordering_dataclass)
-    # FINAL DATA CLASS
-    wrapper = strawberry_django.type(model=model, filters=filter_model, order=order_model, pagination=True)
 
-    return wrapper(new_dataclass), {model.__name__: wrapper(new_dataclass)}
+    return fields_for_class, fields_for_filter, model_name
 
 
 app_models = django.apps.apps.get_models()
-# app_models = [models.Recipe]
+# app_models = [models.Fruit, models.Recipe, models.Color, models.Shape]
 models_list = []
 lowercase_list = []
 wrapped_models_dict = {}
+many_to_many_backwards = []
+
+dict_models_fields_setup = {}
+dict_models_straw_models_setup = {}
+dict_models_fields_filters = {}
 
 for model in app_models:
-    wrapped_model, wrapped_model_dict = model_to_dataclass(model)
+    fields_for_class, fields_for_filter, model_name = create_fields(model, set_relations=False)
+    dict_models_fields_setup[model] = fields_for_class
+    dict_models_fields_filters[model] = fields_for_filter
+    wrapped_model, wrapped_model_dict = create_strawberry_model(
+        fields_for_class=fields_for_class,
+        fields_for_filter=fields_for_filter,
+        strawberry_model_name=model_name,
+        django_model=model,
+    )
+    dict_models_straw_models_setup[model] = wrapped_model
     models_list.append(wrapped_model)
     lowercase_list.append(model._meta.model_name)
     wrapped_models_dict.update(wrapped_model_dict)
 
-models_list = []
-for model in app_models:
-    wrapped_model, _ = model_to_dataclass(model, set_relations=True)
-    models_list.append(wrapped_model)
+for model in dict_models_fields_setup.keys():
+    for model_field in model._meta.get_fields():
+        relation = model_field.__dict__.get('related_model', None)
+
+        if relation:
+            related_model = model_field.related_model
+            straw_model = dict_models_straw_models_setup[related_model]
+            dict_models_fields_setup[model][model_field.name] = List[straw_model]
+
+dict_models_fields_final = {}
+dict_models_straw_models_final = {}
+final_models_list = []
+for model, fields in dict_models_fields_setup.items():
+    fields_for_class, fields_for_filter, model_name = create_fields(model, set_relations=False)
+    wrapped_model, wrapped_model_dict = create_strawberry_model(
+        fields,
+        dict_models_fields_filters[model],
+        model.__name__,
+        model
+    )
+    dict_models_straw_models_final
+    final_models_list.append(wrapped_model)
+
+
 
 list_of_tuples_for_query = []
 for index in range(len(lowercase_list)):
     filed_name = _plural_from_single(lowercase_list[index])
-    field_type = List[models_list[index]]
+    field_type = List[final_models_list[index]]
     list_of_tuples_for_query.append((filed_name, field_type, field(default=strawberry_django.field())))
 wrapper_func = strawberry.type
 Query = make_dataclass("Query", list_of_tuples_for_query)
--- apps/graphql_app/types.py
+++ apps/graphql_app/types.py
@@ -1,44 +1,41 @@
-import strawberry_django
+from typing import List
+
+import strawberry
 from strawberry import auto
+
+import strawberry_django
+from django.contrib.auth import get_user_model
+
 from . import models
 
 
-@strawberry_django.filter(models.Fruit)
-class FruitFilter:
-    id: auto
-    name: auto
-#
-#
-# @strawberry_django.order(models.Color)
-# class ColorOrder:
-#     id: auto
-#     name: auto
-#
-#
-# @strawberry_django.order(models.Fruit)
-# class FruitOrder:
-#     id: auto
-#     name: auto
-#     # color: ColorOrder
+def magic(class_):
+    cls = class_
+    def new_field(self) -> List['Recipe']:
+        return models.Recipe.objects.filter(**{f'fruits__in':[self.id]})
+    setattr(cls, 'recipes', strawberry_django.field(new_field))
+    return cls  # type: ignore
 
 
-@strawberry_django.type(models.Fruit, filters=FruitFilter)
+@strawberry_django.type(
+    models.Fruit,
+    pagination=True,
+)
+@magic
 class Fruit:
     id: auto
     name: auto
-    color: auto
-    shape: auto
+    recipes: List['Recipe']
+    # recipes: list['Recipe'] = strawberry.field(resolver=get_recipes)
+    # @strawberry_django.field
+    # def recipes(self) -> List['Recipe']:
+    #     return models.Recipe.objects.filter(fruits__in=[self.id])
 
-
-@strawberry_django.type(models.Color)
-class Color:
+@strawberry_django.type(
+    models.Recipe,
+    pagination=True,
+)
+class Recipe:
     id: auto
-    name: auto
-    fruits: list['Fruit']
-#
-#
-# @strawberry_django.type(models.Shape)
-# class Shape:
-#     id: auto
-#     name: auto
-#     fruits: list['Fruit']
+    recipe_name: auto
+    fruits: List[Fruit]
